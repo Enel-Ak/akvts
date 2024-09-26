66e497a0d4d5d58775a963d49ef4dc8275b0375b
zhaok <kiooo91@vip.qq.com>
2024-09-26 11:51:05 +0800

    fix

--- src/components/Labels.vue
+++ src/components/Labels.vue
@@ -22,7 +22,7 @@ const router = useRouter()
 const items = ref([])
 const current = ref(null)
 const prev = ref(null)
-const buttonWidth = computed(() => `${100 / props.max - 30 / props.max / 2}%`)
+const buttonWidth = computed(() => `${100 / props.max - 100 / 30 / props.max}%`)
 const h = computed(() => {
 	let _h = props.height
 	if (typeof _h === 'number' || (typeof _h === 'string' && _h.indexOf('px') === -1)) {
