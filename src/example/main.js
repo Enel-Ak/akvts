29639161fcc770bbc847b11e2db3da03aba3343f
zhaok <kiooo91@vip.qq.com>
2024-11-16 17:21:24 +0800

    fix

--- src/example/main.js
+++ src/example/main.js
@@ -5,7 +5,6 @@ import {directives} from '@/directive/index'
 
 import App from './App.vue'
 import akvts from '../index.js'
-// import 'virtual:uno.css'
 
 const app = createApp(App)
 const pinia = createPinia()
