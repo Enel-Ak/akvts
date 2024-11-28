28735bbdfe890e7c197fb4a9107e4d9225c0647b
zhaok <kiooo91@vip.qq.com>
2024-11-28 09:44:57 +0800

    fix: Lock组件混淆之后

--- src/components/Lock.vue
+++ src/components/Lock.vue
@@ -1,81 +1,81 @@
 <script setup>
 import {ref} from 'vue'
 
+const props = defineProps({
+	label: {
+		type: String,
+		default: '授权已过期',
+	},
+})
+
 const _0x1A2B = (() => Date['now']())()
-const loadWasm = async () => {
+const _0x4V3E = (_0x1A2B, _0x2B3C) => Math['floor'](_0x1A2B / _0x2B3C)
+const _0x6HJW = (_0xI9T6) => {
+	const {_0xAK98, _0xAK97} = _0xI9T6.exports
+	return {
+		_0x8H9I: _0xAK98,
+		_0xAJBK: _0xAK97,
+	}
+}
+const _0x8QP = async () => {
+	const _0x2B3C = 0x3e8
 	try {
-		const _0x3C4D = 0x3e8
-		const response = await fetch('/akvts.wasm')
-		if (!response.ok) {
-			throw new Error('LoadWasm response was not ok')
+		const _0xYZAB = localStorage.getItem('AKVTS_TOKEN')
+		const _0x4D5E = await fetch('/akvts.wasm')
+		if (!_0x4D5E.ok || !_0xYZAB) {
+			return 0
 		}
 
-		const wasmModule = await WebAssembly.instantiateStreaming(response)
-		const {instance} = wasmModule
-		const validate = instance.exports.validate
-		const memory = instance.exports.memory
+		const _0x6F7G = await WebAssembly.instantiateStreaming(_0x4D5E)
+		const {instance} = _0x6F7G
+		const {_0x8H9I, _0xAJBK} = _0x6HJW(instance)
+		const _0xCLDM = (it, ct) => {
+			const _0xENFO = new TextEncoder()
+			const _0xPGHQ = _0xENFO.encode(it)
+			const _0xRIJS = 0x64
+			const _0xKLTU = new Uint8Array(_0xAJBK.buffer)
+			const _0xMNOP = BigInt(ct)
 
-		const validateInput = (input, ct) => {
-			const encoder = new TextEncoder()
-			const inputBytes = encoder.encode(input)
-			const inputPtr = 100 // 确保与 WAT 中的比较逻辑一致
-			const memoryView = new Uint8Array(memory.buffer)
-			const ctBigInt = BigInt(ct)
-
-			// 将输入字节写入内存的 inputPtr 位置
-			memoryView.set(inputBytes, inputPtr)
-
-			try {
-				return validate(inputPtr, ctBigInt)
-			} catch (error) {
-				console.error('Error validating input:', error)
-				return false
-			}
+			_0xKLTU.set(_0xPGHQ, _0xRIJS)
+			return _0x8H9I(_0xRIJS, _0xMNOP)
 		}
-
-		const _0x5E6F = Math['floor'](_0x1A2B / _0x3C4D)
-
-		const token = localStorage.getItem('AKVTS_TOKEN')
-
-		if (!token) {
-			console.warn('AKVTS_TOKEN 不存在')
-			return false
-		}
-
-		const result = validateInput(token, _0x5E6F)
-		console.log(result === 1 ? 'Valid' : 'Invalid', result)
-		return result
-	} catch (error) {
-		console.error('Error loading wasm:', error)
-		return false
+		const _0xUVWX = _0x4V3E(_0x1A2B, _0x2B3C)
+		return _0xCLDM(_0xYZAB, _0xUVWX)
+	} catch (_0xHIJKL) {
+		return 0
 	}
 }
 
-const isUse = ref(0)
-loadWasm().then((val) => (isUse.value = val))
+const _0xO0l1 = ref(0)
+_0x8QP().then((val) => (_0xO0l1.value = val))
 </script>
 <template>
 	<div class="akvts-lock">
-		<slot v-if="isUse" name="default"></slot>
+		<slot v-if="_0xO0l1" name="default"></slot>
 		<div v-else class="akvts-lock__content">
-			<Icons icon-name="Lock" size="48" color="var(--z-main)" />
-			<span class="message">授权已过期</span>
+			<Icons icon-name="Lock" size="38" color="var(--z-danger)" />
+			<span class="message">{{ label }}</span>
 		</div>
 	</div>
 </template>
 <style scoped lang="scss">
-.akvts-lock__content {
-	align-items: center;
-	display: flex;
-	flex-direction: column;
+.akvts-lock {
 	height: 100%;
-	justify-content: center;
-	padding: 20px;
-	width: calc(100% - 40px);
-	.message {
-		color: var(--z-font-color);
-		font-size: 16px;
-		padding: 10px 0;
+	width: 100%;
+	.akvts-lock__content {
+		align-items: center;
+		display: flex;
+		flex-direction: column;
+		height: 100%;
+		justify-content: center;
+		padding: 20px;
+		width: calc(100% - 40px);
+		.message {
+			color: var(--z-font-color);
+			font-size: 12px;
+			opacity: 0.8;
+			padding: 10px 0;
+		}
 	}
 }
 </style>
