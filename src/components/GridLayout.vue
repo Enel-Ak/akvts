cb1d98eaaf7842930b31de1ed3949b17050cc18e
zhaok <kiooo91@vip.qq.com>
2025-01-14 11:48:25 +0800

    fix: bug

--- src/components/GridLayout.vue
+++ src/components/GridLayout.vue
@@ -1,10 +1,29 @@
 <script setup>
-import VueGridLayout from 'vue-grid-layout'
+import {onMounted} from 'vue'
+import {GridStack} from 'gridstack'
+import 'gridstack/dist/gridstack.min.css'
+
+onMounted(() => {
+	GridStack.init()
+})
 </script>
 <template>
 	<div class="grid-component">
-		123
-		<VueGridLayout></VueGridLayout>
+		<div class="grid-stack">
+			<div class="grid-stack-item">
+				<div class="grid-stack-item-content">Item 1</div>
+			</div>
+			<div class="grid-stack-item" gs-w="2">
+				<div class="grid-stack-item-content">Item 2 wider</div>
+			</div>
+		</div>
 	</div>
 </template>
-<style lang="css" scoped></style>
+<style lang="css" scoped>
+.grid-stack {
+	background: #fafad2;
+}
+.grid-stack-item-content {
+	background-color: #18bc9c;
+}
+</style>
